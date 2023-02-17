808a632bac41a485c07cb49f57072e6648d328a5
ksom-kay <karensakyibea@gmail.com>
2023-02-17 09:19:08 -0800

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -  prints a line to the standard error
- * Return: 1 (Success)
+ * main - Entry
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, 59);
+
 	return (1);
 }
